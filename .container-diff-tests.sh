cdba954951b1c72f9668f76eeccb3d9f35d9b518
Abby Tisdale <atisdale@google.com>
2017-08-16 11:25:38 -0700

    Removing now unneccessary processing

--- .container-diff-tests.sh
+++ .container-diff-tests.sh
@@ -7,14 +7,6 @@ while IFS=$' \n\r' read -r flag differ image1 image2 file; do
   fi
 done < tests/differ_runs.txt
 
-while IFS=$' \n\r' read -r preprocess json; do
-  python $preprocess $json
-  if [[ $? -ne 0 ]]; then
-  echo "Could not preprocess" "$json" "for diff comparison"
-  exit 1
-fi
-done < tests/preprocess_files.txt
-
 success=0
 while IFS=$' \n\r' read -r differ actual expected; do
   diff=$(jq --argfile a "$actual" --argfile b "$expected" -n 'def walk(f): . as $in | if type == "object" then reduce keys[] as $key ( {}; . + { ($key):  ($in[$key] | walk(f)) } ) | f elif type == "array" then map( walk(f) ) | f else f end; ($a | walk(if type == "array" then sort else . end)) as $a | ($b | walk(if type == "array" then sort else . end)) as $b | $a == $b')
